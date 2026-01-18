56409578860c95b2707b36ff05869f2cc17ac54a
elumbantoruan418-collab <elumbantoruan418@gmail.com>
2026-01-18 09:29:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,15 @@
-config.js
-node_modules
+module.exports = {
+  // ================= BOT CONFIG =================
+  botToken: "8552279044:AAH0nEbIDqO72DGC_8VKHNKP6NHnhNVHoeU",
+  ownerId: 8427342247, // HARUS NUMBER (tanpa tanda petik)
+  botPhoto: "https://files.catbox.moe/ydauxn.jpg",
+
+// ================= GITHUB =================
+  github: {
+    token: "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",
+    owner: "elumbantoruan418-collab",
+    repo: "Surat",
+    file: "Premium.json",
+    branch: "main"
+  }
+};
